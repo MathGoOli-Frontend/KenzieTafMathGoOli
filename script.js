f73540161a6823f83aeaf0cfc31394c40492bf31
mathGoOli <matheuss456@hotmail.com>
2023-02-15 12:51:04 -0300

    adicionado configuracoes

--- script.js
+++ script.js
@@ -21,18 +21,47 @@ passed = false;
 // if (gender === "male")  
 // **sua lógica a partir daqui**
 
-if (gender === "male"){ //pra deixar mais rápido eu vou deixar uma else if porque se for reprovado em um já acusa passed false
-    if(height < 1.70){
+//configuração só pra dizer que usei "corretamente sobre o uso de var/let/const"
+// não precisei usar var/let porque var não se usa e let não precisei por que não preciso mudar essas informações durante a execução do script
+// Homem
+const alturaHomem = 1.7
+const absHomem = 41
+const barRepHomem = 6
+const barSecondsHomen = 15
+const swimDistanceHomem = 100
+const swimTimeHomem = 60
+const diveTimeHomem = 30
+const runDistanceHomen1 = 3000
+const runTimeHomen1 = 12*60
+const runDistanceHomen2 = 5000
+const runTimeHomen2 = 20*60
+// Mulher
+// eu sei que algumas constantes são iguais tipo swimTimeHomem e swimTimeMulher mas preferi deixar separado porque na explicação do taf estao separadas
+const alturaMulher = 1.6
+const absMulher = 41
+const barRepMulher = 5
+const barSecondsmulher = 12
+const swimDistanceMulher = 100
+const swimTimeMulher = 60
+const diveTimeMulher = 30
+const runDistanceMulher1 = 4000
+const runTimeMulher1 = 15*60
+const runDistanceMulher2 = 6000
+const runTimeMulher2 = 22*60
+
+if (gender === "male"){ 
+    //pra deixar mais rápido eu vou deixar uma else if porque se for reprovado em um já acusa passed == false
+    if(height < alturaHomem){ // Altura
         passed = false
-    }else if (abs < 41){
+    }else if (abs < absHomem){ // abnominais
         passed = false
     }else {
         passed = true
     }
-    // bar reps
+    //  repeticoes da barra
     if (passed == true){
-        if(barReps < 6){
-            if (barSeconds >= 15){
+        if(barReps < barRepHomem){
+            if (barSeconds >= barSecondsHomen){
                 passed = true
             }
         }else{
@@ -43,9 +72,9 @@ if (gender === "male"){ //pra deixar mais rápido eu vou deixar uma else if porq
     
     // natacao
     if (passed == true){
-        if(swimDistance >= 100 && swimTime <= 60){
+        if(swimDistance >= swimDistanceHomem && swimTime <= swimTimeHomem){
             passed = true
-        }else if(diveTime <= 30){
+        }else if(diveTime <= diveTimeHomem){
             passed = true
         }else{
             passed = false
@@ -55,14 +84,14 @@ if (gender === "male"){ //pra deixar mais rápido eu vou deixar uma else if porq
 
     // corrida
     if (passed == true){
-        if(runDistance >= 3000){
-            if (runTime <= 720){
+        if(runDistance >= runDistanceHomen1){ // 3km já convertido pro script não precisar fazer a conta # * 1000
+            if (runTime <= runTimeHomen1){
                 passed = true
             }else{
                 passed = false
             }
-        }else if(runDistance >= 5000){
-            if (runTime <= 1200){
+        }else if(runDistance >= runDistanceHomen2){
+            if (runTime <= runTimeHomen2){
                 passed = true
             }else{
                 passed = false
@@ -76,17 +105,17 @@ if (gender === "male"){ //pra deixar mais rápido eu vou deixar uma else if porq
     
 
 }else if (gender === "female"){
-    if(height < 1.60){
+    if(height < alturaMulher){
         passed = false
-    }else if (abs < 41){
+    }else if (abs < absMulher){
         passed = false
     }else {
         passed = true
     }
     // bar reps
     if (passed == true){
-        if(barReps < 5){
-            if (barSeconds >= 12){
+        if(barReps < barRepMulher){
+            if (barSeconds >= barSecondsmulher){
                 passed = true
             }
         }else{
@@ -97,9 +126,9 @@ if (gender === "male"){ //pra deixar mais rápido eu vou deixar uma else if porq
     
     // natacao
     if (passed == true){
-        if(swimDistance >= 100 && swimTime <= 60){
+        if(swimDistance >= swimDistanceMulher && swimTime <= swimTimeMulher){
             passed = true
-        }else if(diveTime <= 30){
+        }else if(diveTime <= diveTimeMulher){
             passed = true
         }else{
             passed = false
@@ -109,14 +138,14 @@ if (gender === "male"){ //pra deixar mais rápido eu vou deixar uma else if porq
 
     // corrida
     if (passed == true){
-        if(runDistance >= 4000){
-            if (runTime <= 900){
+        if(runDistance >= runDistanceMulher1){
+            if (runTime <= runTimeMulher1){
                 passed = true
             }else{
                 passed = false
             }
-        }else if(runDistance >= 6000){
-            if (runTime <= 1320){
+        }else if(runDistance >= runDistanceMulher2){
+            if (runTime <= runTimeMulher2){
                 passed = true
             }else{
                 passed = false
